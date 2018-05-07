fb88a6760604f586cae5d5ad65b07e1cbaedb647
Mercer08572 <sun656105944@outlook.com>
2018-05-07 18:08:16 +0800

    # 2018年5月7日18:07:27 回去后将参数修改为request，添加头信息

--- mp4Crawler/scheduler/WebPageDownloader.py
+++ mp4Crawler/scheduler/WebPageDownloader.py
@@ -3,6 +3,17 @@
 # datatime : 2018/5/3 17:12
 # author : badbugu17
 # file : WebPageDownloader
+import urllib
+
 
 class WebPageDownloader:
     """网页下载器"""
+
+    def htmlDownload(self,new_url):
+        if new_url is not None:
+            response = urllib.request.urlopen(new_url);
+            if response.getcode() != 200:
+                return None;
+            return response.read();
+        else:
+            return None;
