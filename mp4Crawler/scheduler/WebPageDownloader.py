919a7abeabf63c9c00e54ea3a0419d9cb0fa0336
Mercer08572 <sll+8262340926>
2018-05-08 00:41:57 +0800

    # 2018年5月8日00:40:42 将urllib.request下载网页的参数修改为request格式

--- mp4Crawler/scheduler/WebPageDownloader.py
+++ mp4Crawler/scheduler/WebPageDownloader.py
@@ -3,7 +3,7 @@
 # datatime : 2018/5/3 17:12
 # author : badbugu17
 # file : WebPageDownloader
-import urllib
+import urllib.request
 
 
 class WebPageDownloader:
@@ -11,7 +11,18 @@ class WebPageDownloader:
 
     def htmlDownload(self,new_url):
         if new_url is not None:
-            response = urllib.request.urlopen(new_url);
+            request = urllib.request.Request(new_url);
+            # 伪装成Chrome浏览器
+            request.add_header("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36");
+            request.add_header("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+            # request.add_header("Accept-Encoding","gzip, deflate");
+            request.add_header("Accept-Language","zh-CN,zh;q=0.9");
+            request.add_header("Cache-Control","max-age=0");
+            request.add_header("Connection","keep-alive");
+            request.add_header("Host","www.mp4ba.net");
+            request.add_header("Upgrade-Insecure-Requests","1");
+
+            response = urllib.request.urlopen(request);
             if response.getcode() != 200:
                 return None;
             return response.read();
